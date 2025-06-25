70f2fe41db0abe85ae722c4f336cb6342e9d20f0
deimos715 <deimos715@gmail.com>
2025-06-25 20:57:53 +0300

    add template geektop

--- geektop/app/js_src/main.js
+++ geektop/app/js_src/main.js
@@ -3,6 +3,16 @@
 */
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+/*
+ * Bootstrap 5 Accardion
+*/
+// import { Collapse } from 'bootstrap';
+
+/*
+ * Bootstrap 5 Alert
+*/
+// import { Alert } from 'bootstrap';
+
 
 /*
  * My app
